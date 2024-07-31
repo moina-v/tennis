3e8dc54589782b968677c404b76725eb35c32089
moina-v <moinawaliyat.ali@gmail.com>
2024-07-31 14:58:09 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+window.onload = function(){
+	alert("Hahaha, ton site a été piraté, pauvre joueuse de tennis !");
+	alert("si tu réponds correctement à la question suivante, tu pourras peut-être me retrouver...");
+	let question = "";
+	while(question != "5") {
+		question = prompt("Combien y-a-t-il de joueurs dans une équipe de basket ?");
+	}
+	alert("bravo, tu as trouvé la bonne réponse, tu peux éliminer toutes les personnes du rang 5 !");
+	alert("Il te reste 1 indice à trouver. Inspecte un peu ton site pour trouver la suite !");
+	console.log("Basket Unity");
+}
+
